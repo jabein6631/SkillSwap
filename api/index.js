2c6132f8ea0a4fd5fd3ae7d4cd4c32b80284fad7
jabein6631 <jabein6631@users.noreply.github.com>
2026-09-25 21:35:37 +0530

    Simplify api/index.js entrypoint to eliminate 10s serverless timeout

--- api/index.js
+++ api/index.js
@@ -2,23 +2,5 @@
  * SkillSwap Platform - Vercel Serverless Express Entrypoint
  */
 const { app } = require('../backend/server');
-const { initSchema } = require('../backend/database/db');
-const { seedDatabase } = require('../backend/database/seed');
-
-let isInitialized = false;
-
-// Middleware for lazy initialization of Turso Cloud DB on Vercel
-app.use(async (req, res, next) => {
-  if (!isInitialized) {
-    try {
-      await initSchema();
-      await seedDatabase();
-    } catch (err) {
-      console.error('[Vercel Serverless Init Notice]:', err.message);
-    }
-    isInitialized = true;
-  }
-  next();
-});
 
 module.exports = app;
